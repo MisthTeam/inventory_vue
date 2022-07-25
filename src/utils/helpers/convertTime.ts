1f51832140f323cf2fbf5ef66891a653f59e0a51
Slincnik <pattyshow21@mail.ru>
2022-07-25 14:05:07 +0300

    Fix date

--- src/utils/helpers/convertTime.ts
+++ src/utils/helpers/convertTime.ts
@@ -2,7 +2,7 @@ import { format } from "date-fns";
 import { ru } from "date-fns/locale";
 
 const convertTime = (time: string) =>
-  format(new Date(time), "dd.mm.yy", {
+  format(new Date(time), "dd.MM.yy", {
     locale: ru,
   });
 

4e98b8841353b6a86ac364a84f5019b4059d1770
Slincnik <pattyshow21@mail.ru>
2022-10-27 17:40:27 +0300

    Fix devices list

--- src/components/Admin/Devices/DevicesList.vue
+++ src/components/Admin/Devices/DevicesList.vue
@@ -16,40 +16,24 @@
     </tbody>
   </table>
   <BaseModal name="Изменение устройства" :isOpen="isOpenModal" @close="modalCancel" @ok="modalOk">
-    <template v-if="isOpenModal" #default>
-      <template v-if="dto">
-        <div class="form-floating mb-3">
-          <input id="floatingInput2" v-model="dto.pn" type="text" class="form-control" placeholder="P/N" />
+    <template #default>
+      <div class="form-floating mb-3">
+        <input id="floatingInput2" v-model="dto.pn" type="text" class="form-control" placeholder="P/N" />
 
-          <label for="floatingInput2">P/N</label>
-        </div>
+        <label for="floatingInput2">P/N</label>
+      </div>
 
-        <div class="form-floating mb-3">
-          <select v-model="dto.type" class="form-select" aria-label="Device type">
-            <option
-              v-for="_type in deviceTypes"
-              :key="_type.id"
-              :value="_type.type"
-              :selected="_type.type === dto.type"
-            >
-              {{ _type.type }}
-            </option>
-          </select>
-          <label for="floatingInput">Device type</label>
-        </div>
+      <div class="form-floating mb-3">
+        <select v-model="dto.type" class="form-select" aria-label="Device type">
+          <option v-for="_type in deviceTypes" :key="_type.id" :value="_type.type" :selected="_type.type === dto.type">
+            {{ _type.type }}
+          </option>
+        </select>
+        <label for="floatingInput">Device type</label>
+      </div>
 
-        <div class="input-group justify-content-between mb-3">
-          <SpecificationsList :type="dto.type" :device="dto" :dto="dto" @editSpecification="changeSpecification" />
-        </div>
-      </template>
-    </template>
-    <template v-else #default>
-      <div class="placeholder-glow" aria-hidden="true">
-        <div><input class="placeholder col-12" /></div>
-        <div><select class="placeholder col-12"></select></div>
-        <div>
-          <span class="placeholder col-12"></span>
-        </div>
+      <div v-if="dto.specification" class="input-group justify-content-between mb-3">
+        <SpecificationsList :type="dto.type" :device="dto" :dto="dto" @editSpecification="changeSpecification" />
       </div>
     </template>
   </BaseModal>
@@ -70,11 +54,9 @@ const { editing } = editDevice();
 
 const isOpenModal = ref(false);
 const dto = ref<Device>({} as Device);
-const device = ref<Device>({} as Device);
 
-const openModal = (d: Device) => {
-  dto.value = { ...d };
-  device.value = d;
+const openModal = (device: Device) => {
+  dto.value = JSON.parse(JSON.stringify(device));
   isOpenModal.value = true;
 };
 
@@ -89,8 +71,8 @@ const modalOk = () => dto.value && editing(dto.value); // Изменение д�
 
 watch(
   () => dto.value.type,
-  async (_, oldValue) => {
-    if (oldValue) {
+  async (newValue, oldValue) => {
+    if (newValue && oldValue) {
       dto.value && (dto.value.specification = {});
     }
   },

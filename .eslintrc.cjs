e7f79753677ed49c1bb6d86efb13a00a81750368
Slincnik <pattyshow21@mail.ru>
2023-01-16 21:14:31 +0300

    Обновил зависимости

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,12 +9,16 @@ module.exports = {
     ecmaVersion: 2020,
     sourceType: "module",
   },
-  extends: ["plugin:@typescript-eslint/recommended", "plugin:vue/vue3-recommended", "plugin:prettier/recommended"],
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "plugin:vue/vue3-recommended",
+    "plugin:prettier/recommended",
+    "@vue/typescript/recommended",
+  ],
   plugins: ["prettier", "@typescript-eslint"],
   rules: {
     "vue/valid-attribute-name": "off",
     "prettier/prettier": "warn",
-    "no-unused-vars": "off",
     "vue/v-on-event-hyphenation": "off",
     "@typescript-eslint/no-unused-vars": ["off"],
     "vue/attribute-hyphenation": "off",
